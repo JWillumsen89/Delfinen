75dba5b8877d140b42528c2fe327e5d3535f12dd
Arhqirah <Simonmelbyeandersen@hotmail.com>
2022-05-11 11:12:27 +0200

    added Main & Program

--- src/Delfin/Main.java
+++ src/Delfin/Main.java
@@ -1,4 +1,4 @@
-package src.Main;
+package Delfin;
 
 public class Main {
     public static void main(String[] args) {
--- src/Delfin/Program.java
+++ src/Delfin/Program.java
@@ -1,4 +1,4 @@
-package src.Main;
+package Delfin;
 
 public class Program {
 
