fa9a1f6d5b25f18e0319b2b8bf5ec9da2733d362
Arhqirah <Simonmelbyeandersen@hotmail.com>
2022-05-16 14:37:08 +0200

    Member(test)

--- src/Members/Member.java
+++ src/Members/Member.java
@@ -136,7 +136,7 @@ public class Member {
         "\nAge: " + age +
         "\nPhoneNumber: " + phoneNumber +
         "\nActive(A) or Passive(P): " + activeOrPassive +
-        "\nPaid(P) or Not(N): test" + paidOrNot +
+        "\nPaid(P) or Not(N):" + paidOrNot +
         '\n';
 
 
