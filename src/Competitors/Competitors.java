5d9a253178b44cce38eb2524a85f9349df211692
Arhqirah <Simonmelbyeandersen@hotmail.com>
2022-05-17 10:05:29 +0200

    adding Competitors made ArrayLists Disciplines, training score and competitions

--- src/Competitors/Competitors.java
+++ src/Competitors/Competitors.java
@@ -22,5 +22,19 @@ public class Competitors extends Member {
                        ArrayList<Competition>competitions) {
 
         super(name, age, phoneNumber, email, memberID, activeOrPassive, paidOrNot);
+
+
     }
+// ----------------------------------SETTER--------------------------------------------
+
+    public void setDisciplines(ArrayList<Discipline> disciplines) {
+        this.disciplines = disciplines;
+    }
+    public void setTrainingScores(ArrayList<TrainingScore> trainingScores) {
+        this.trainingScores = trainingScores;
+    }
+    public void setCompetitions(ArrayList<Competition> competitions) {
+        this.competitions = competitions;
+    }
+
 }
